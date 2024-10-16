95ff8f8b672e9f2576e663e279597b21ae877aa2
aintnewtathis-yy <aintnewtathis@mail.ru>
2024-10-16 12:31:19 +0300

    navigate scrollTo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,16 @@
 	import '@splidejs/svelte-splide/css/core';
 	import Header from '$lib/components/global/Header.svelte';
 
+	import { afterNavigate } from '$app/navigation';
 
 	let { data, children } = $props();
 
-	
+	afterNavigate(() => {
+		window.scrollTo(0)
+	});
 </script>
-
 
 <Header />
 <main class="mt-[76px] max-md:mt-0">
 	{@render children()}
 </main>
-
-
